ae9bd65106355b980783a708edac245adea8829a
yonatan.perel@pointfive.co <yonatan.perel@pointfive.co>
2025-08-11 15:50:43 +0300

    updates to nvim config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "main",
-  build = ":TSUpdate",
-  opts = {
-    ensure_installed = { "lua", "vim", "vimdoc", "sql", "javascript", "html", "python", "go" },
-    sync_install = false,
-    highlight = { enable = false },
-    indent = { enable = false },
-  },
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+    opts = {
+        ensure_installed = { "lua", "vim", "vimdoc", "sql", "javascript", "html", "python", "go" },
+        sync_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+    },
 }

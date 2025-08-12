27e0316cccc903baadad867f64eb282f828aef34
yonatan.perel@pointfive.co <yonatan.perel@pointfive.co>
2025-08-12 13:08:47 +0300

    remvoed filetypes

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,4 +1,3 @@
 _G.lsps = { "gopls", "lua_ls", "pyright", "ts_ls", "bashls" }
 _G.languages = { "go", "lua", "python", "sql", "bash" }
-_G.filetypes = { "go", "gomod", "gowork", "gotmpl", "lua", "sh", "bash", "sql", "py" }
 vim.lsp.enable(_G.lsps)
